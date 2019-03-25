f88b8764ba44874402cd9c93805c4863091fc217
Nathan Jen <njen@andrew.cmu.edu>
2019-03-24 21:41:02 -0400

    list page now directs instances to separate show page when clicked

--- tep2019_code/djangular/src/app/app-routing.module.ts
+++ tep2019_code/djangular/src/app/app-routing.module.ts
@@ -5,12 +5,18 @@ import { TeacherListComponent } from './teacher-list/teacher-list.component';
 import { OrderListComponent } from './order-list/order-list.component';
 import { OrderItemListComponent } from './order-item-list/order-item-list.component';
 import { SchoolListComponent } from './school-list/school-list.component';
+import { TeacherDetailComponent } from './teacher-detail/teacher-detail.component';
+import { OrderDetailComponent } from './order-detail/order-detail.component';
+import { ItemDetailComponent } from './item-detail/item-detail.component';
 
 const routes: Routes = [
   { path: 'items', component: ItemListComponent },
+  { path: 'items/:id', component: ItemDetailComponent },
   { path: 'teachers', component: TeacherListComponent },
+  { path: 'teachers/:id', component: TeacherDetailComponent },
   { path: 'order-items', component: OrderItemListComponent },
   { path: 'orders', component: OrderListComponent },
+  { path: 'orders/:id', component: OrderDetailComponent },
   { path: 'schools', component: SchoolListComponent }
 ];
 
