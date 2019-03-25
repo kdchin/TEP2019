6b5e3634ee36c981a8431f5047624015d1d0fd06
Joe Segel <4segel@gmail.com>
2019-03-24 21:07:44 -0400

    doesnt work at all

--- tep2019_code/djangular/src/app/app-routing.module.ts
+++ tep2019_code/djangular/src/app/app-routing.module.ts
@@ -5,13 +5,19 @@ import { TeacherListComponent } from './teacher-list/teacher-list.component';
 import { OrderListComponent } from './order-list/order-list.component';
 import { OrderItemListComponent } from './order-item-list/order-item-list.component';
 import { SchoolListComponent } from './school-list/school-list.component';
+import { TeacherDetailComponent } from './teacher-detail/teacher-detail.component';
+
+
+
+
 
 const routes: Routes = [
   { path: 'items', component: ItemListComponent },
   { path: 'teachers', component: TeacherListComponent },
   { path: 'order-items', component: OrderItemListComponent },
   { path: 'orders', component: OrderListComponent },
-  { path: 'schools', component: SchoolListComponent }
+  { path: 'schools', component: SchoolListComponent },
+  { path: 'teacher-detail', component: TeacherDetailComponent}
 ];
 
 @NgModule({
