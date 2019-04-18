86453a464ce6d3fe9cf7a3dbb71780fe920521ad
Joe Segel <4segel@gmail.com>
2019-04-18 19:19:28 -0400

    pagination added to all lists pages

--- tep2019_code/djangular/src/app/teacher-detail/teacher-detail.component.ts
+++ tep2019_code/djangular/src/app/teacher-detail/teacher-detail.component.ts
@@ -1,12 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Location } from '@angular/common';
-import { Order } from '../models';
 
 import { Teacher } from '../models';
 import { ApiService }  from '../api.service';
-import { PhonePipe } from '../phone.pipe';
-import { BoolPipe } from '../bool.pipe';
+import { ResourceLoader } from '@angular/compiler';
 
 @Component({
   selector: 'app-teacher-detail',
@@ -15,66 +13,28 @@ import { BoolPipe } from '../bool.pipe';
 })
 export class TeacherDetailComponent implements OnInit {
   teacher: Teacher;
-
-  selectedOrder: Order;
-
-  private order: Array<Order> = [];
-  // private newOrders: Array<Order> = [];
-  private orders: Array<Order> = [];
-  private phonePipe = new PhonePipe();
-  private activePipe = new BoolPipe();
  
   constructor(
     private route: ActivatedRoute,
     private apiService: ApiService,
     private location: Location
-    
   ) { }
  
   ngOnInit() {
     this.getTeacher();
-    this.getOrders();
   }
 
   getTeacher(): void {
     const id = +this.route.snapshot.paramMap.get('id');
     this.apiService.fetchOne("teachers", id).subscribe((teacher: Teacher) => this.teacher = teacher);
+  }
 
-    // this.orders2 = this.objects.orders;
+  deleteTeacher(teacher: Teacher) {
+    this.apiService.delete("teachers", this.teacher.id).subscribe();
+    this.location.back();
   }
 
   goBack(): void {
     this.location.back();
-  }
-
-  public onSelect(order: Order) {
-    this.selectedOrder = order;
-  }
-
-  // public deleteOrder(order: Order) {
-  //   let new_orders = this.orders.filter(oi => oi.id !== order.id);
-  //   // TODO: error and warn
-  //   if (new_orders.length != this.orders.length - 1)
-  //     return;
-  //   this.orders = new_orders;
-  //   this.apiService.delete("orders", order.id).subscribe();
-  // }
-
-  public getOrders() {
-    const id = +this.route.snapshot.paramMap.get('id');
-
-    this.apiService.fetchAll("orders").subscribe((data: Array<Order>) => {
-      this.orders = data;
-    }) 
-     
-
-
-    // for (let order of this.orders) {
-    //   if (order.teacher.email ==  this) {
-    //     this.newOrders.push(order);
-    //   }
-    // }
-    
-    // this.orders = this.newOrders;
   }
 }
