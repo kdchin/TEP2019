f4ca17b4892529dda486838bf936faba7e83eea7
kyle <kdchin@andrew.cmu.edu>
2019-04-22 02:59:31 -0400

    can update teacher on teacherdetail now

--- tep2019_code/djangular/src/app/teacher-detail/teacher-detail.component.ts
+++ tep2019_code/djangular/src/app/teacher-detail/teacher-detail.component.ts
@@ -4,6 +4,8 @@ import { Location } from '@angular/common';
 
 import { TeacherDetail } from '../models';
 import { ApiService } from '../api.service';
+import { PhonePipe } from '../phone.pipe';
+import { BoolPipe } from '../bool.pipe';
 
 @Component({
   selector: 'app-teacher-detail',
@@ -12,6 +14,8 @@ import { ApiService } from '../api.service';
 })
 export class TeacherDetailComponent implements OnInit {
   teacher: TeacherDetail;
+  phonePipe = new PhonePipe();
+  activePipe = new BoolPipe();
 
   constructor(
     private route: ActivatedRoute,
@@ -31,6 +35,13 @@ export class TeacherDetailComponent implements OnInit {
   deleteTeacher(teacher: TeacherDetail) {
     this.apiService.delete("teachers", this.teacher.id).subscribe();
     this.location.back();
+  }
+
+  updateTeacher(attr: string) {
+    return (newValue) => {
+      this.teacher[attr] = newValue;
+      this.apiService.update('teacher_update', this.teacher).subscribe();
+    }
   }
 
   goBack(): void {
